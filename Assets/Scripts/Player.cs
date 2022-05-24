bef1da2e97db9454a83fae4d6fd3fccd89e262ba
kdgkt123 <42763934+kdgkt123@users.noreply.github.com>
2022-05-25 00:17:29 +0900

    add Jump
    AddForce 함수를 사용한 점프 기능 구현

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,19 +7,49 @@ public class Player : MonoBehaviour
 
     public float moveSpd; // 플레이어가 이동하는 속도
     private Rigidbody2D rigidbody;
+    public float jmpForce;
+
+    private bool isJumping;
     // Start is called before the first frame update
     void Start()
     {
+        isJumping = false;
         rigidbody = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
+    {
+        Move();
+        Jump();
+    }
+
+    void Move()
     {
         float hAxis = Input.GetAxisRaw("Horizontal");
 
-        Vector2 inputDir = new Vector2(hAxis,0).normalized;
+        Vector2 inputDir = new Vector2(hAxis, 0).normalized;
 
-        rigidbody.velocity = inputDir * moveSpd;
+        this.transform.Translate(inputDir * moveSpd * Time.deltaTime);
+    }
+
+    void Jump()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            if (!isJumping)
+            {
+                isJumping = true;
+                rigidbody.AddForce(Vector2.up*jmpForce, ForceMode2D.Impulse);
+            }
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isJumping = false;
+        }
     }
 }
