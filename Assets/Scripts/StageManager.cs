f0e083ffb4eff64d4b3e7e452cc17946076bb046
teronalision <43131617+teronalision@users.noreply.github.com>
2022-05-27 19:06:48 +0900

    맵과 오브젝트 활성화 연동

--- Assets/Scripts/StageManager.cs
+++ Assets/Scripts/StageManager.cs
@@ -5,18 +5,22 @@ using UnityEngine;
 public class StageManager : MonoBehaviour
 {
 
-    public Transform[] stages;
+    public Transform Objects;
 
     public Transform player_pos;
 
     public int map_distance = 20;
 
+    private Stage[] stagelist;
 
     public void LoadingMap()
     {
-        foreach (var item in stages)
+        foreach (var item in stagelist)
         {
-            item.parent.gameObject.SetActive(map_distance > Vector2.Distance(item.position, player_pos.position));
+            bool b = map_distance > Vector2.Distance(item.pos.position, player_pos.position);
+
+            item.gameObject.SetActive(b);
+            item.tile.SetActive(b);
         }
     }
 
@@ -25,7 +29,7 @@ public class StageManager : MonoBehaviour
 
     void Start()
     {
-        
+        stagelist = Objects.GetComponentsInChildren<Stage>();
     }
 
     // Update is called once per frame
---
+++ Assets/Stage.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Stage : MonoBehaviour
+{
+
+    public Transform pos;
+    public GameObject tile;
+
+
+    void Start()
+    {
+        
+    }
+
+   
+}
